34001445092c906db69c36192adad7c2ff5cada5
Andrii Isaiev <abiexpect@yahoo.com>
2022-02-20 17:02:27 +0200

    for for for

--- src/main/java/com/abi/Model.java
+++ src/main/java/com/abi/Model.java
@@ -18,6 +18,16 @@ public class Model {
     }
 
     public static void main(String[] args) {
+        Model m = new Model();
+        System.out.println(m.matrix[0][0] + " " + m.matrix[0][1001] + " " + m.matrix[1001][1001] + " " + m.matrix[1001][0]);
+        for (int n = 1; n <= 100 ; n++) {
+            for (int j = 1; j <= 1000; j++) {
+                for (int i = 1; i <= 1000; i++) {
+                    m.matrix[j][i] = (m.matrix[j - 1][i] + m.matrix[j + 1][i] + m.matrix[j][i - 1] + m.matrix[j][i + 1]) /4;
+                }
 
+            }
+        }
+        System.out.println(m.matrix[100][100] + " " + m.matrix[500][500] + " " + m.matrix[100][900] + " " + m.matrix[900][500]);
     }
 }
